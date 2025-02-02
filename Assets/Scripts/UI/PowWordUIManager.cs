cdbc25607fb6b21ac6b6f20ff171c2c04bfc9bf4
akoreyu <ym433@drexel.edu>
2025-02-02 18:22:07 -0500

    Special Weapons Text
    When you get a special combination, a text animation pops up

--- Assets/Scripts/UI/PowWordUIManager.cs
+++ Assets/Scripts/UI/PowWordUIManager.cs
@@ -2,6 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
+using TMPro;
+using DG.Tweening;
 
 public class PowWordUIManager : MonoBehaviour
 {
@@ -9,6 +11,7 @@ public class PowWordUIManager : MonoBehaviour
     [SerializeField] GameObject bulletPrefab;
     public GameObject bulletsParent;
     [SerializeField] public List<DropZone> dropZones;
+    [SerializeField] TextMeshProUGUI specialsText;
     private string currentPow;
     public GameObject gunMenu;
     public Transform hudPosition;
@@ -85,6 +88,11 @@ public class PowWordUIManager : MonoBehaviour
         currentPow = GetCurrentPowString();
         PowerGameState.Instance.PowString = currentPow;
 
+        if (specialsText)
+        {
+            CheckSpecials();
+        }
+
         if (dropZones.Last().isLoaded && dropZones.Count < 5)
         {
             CreateBlankBullet();
@@ -145,5 +153,71 @@ public class PowWordUIManager : MonoBehaviour
         }
         currentLength++;
         OnChange();
+    }
+
+    public void CheckSpecials()
+    {
+        if (currentPow == "POWPOWPOWPOWPOW")
+        {
+            StartCoroutine(SpecialsTextAnimation("MACHINE GUN"));
+        }
+        if (currentPow.Contains("PPPPPPPPPP"))
+        {
+            StartCoroutine(SpecialsTextAnimation("RAILGUN"));
+        }
+        if (currentPow.Contains("POOWW"))
+        {
+            foreach (DropZone zone in dropZones)
+            {
+                if (zone.currentCaseString == "POOWW") StartCoroutine(SpecialsTextAnimation("DOUBLE BARREL"));
+            }
+        }
+        if (currentPow.Contains("PPOOW"))
+        {
+            foreach (DropZone zone in dropZones)
+            {
+                if (zone.currentCaseString == "PPOOW") StartCoroutine(SpecialsTextAnimation("SONIC SHOT"));
+            }
+        }
+        if (currentPow.Contains("OOOOOOOOOO"))
+        {
+            StartCoroutine(SpecialsTextAnimation("QUANTUM CANNON"));
+        }
+        if (currentPow.Contains("WWWWWWWWWW"))
+        {
+            StartCoroutine(SpecialsTextAnimation("ANNIHILATOR"));
+        }
+    }
+
+    IEnumerator SpecialsTextAnimation(string special)
+    {
+        specialsText.transform.localScale = Vector3.one;
+        specialsText.text = special;
+        specialsText.gameObject.SetActive(true);
+
+        specialsText.transform.DOScale(Vector3.one, 0.25f)  // Animate scale to its original size
+            .OnStart(() =>
+            {
+                // Shake the object immediately when it starts appearing
+                specialsText.transform.DOShakeScale(0.3f, 1f);
+            })
+            .OnComplete(() =>
+            {
+                // After appearing, wait for the disappear delay before starting the disappearing shake
+                DOVirtual.DelayedCall(2.5f, () =>
+                {
+                    // Shake and disappear (animate the scale to 0 to make it disappear)
+                    specialsText.transform.DOShakeScale(0.25f, 1f)
+                        .OnComplete(() =>
+                        {
+                            // Set scale to 0 after shaking to make the object disappear
+                            specialsText.transform.localScale = Vector3.zero;
+                        });
+                });
+            });
+
+        yield return new WaitForSeconds(3f);
+
+        specialsText.gameObject.SetActive(false);
     }
 }
